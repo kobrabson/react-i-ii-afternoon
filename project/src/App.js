36324937fb1aab5ed24a09a7a01a3fb97aea5bce
kobrabson <67922399+kobrabson@users.noreply.github.com>
2020-07-23 15:24:32 -0600

    app.js&List.js setup

--- project/src/App.js
+++ project/src/App.js
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import data from './data'
+import data from './data';
+import List from './Components/List'
 
-function App() {
-  return (
-    <div className="App">
-    working
+
+// const App = () => {
+//   return(
+//     <div className="App">
+//     <h1 className="name">name</h1>
+    
+// </div>
+//   )
+// }
+
+
+class App extends Component {
+  constructor () {
+    super()
+
+  this.state = {
+
+    }
+  }
+
+render(){
+  return(
+    <div className='App' >
+      <h1>Name</h1>
     </div>
-  );
+  )
 }
+
+}
+
+
 
 export default App;
---
+++ project/src/Components/List.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import data from '../data';
+import ListItem from './ListItem';
+
+class List extends Component{
+    constructor(){
+        super()
+
+        this.state ={
+
+        }
+    }
+
+    render(){
+        return(
+            <div>List.js</div>
+        )
+    }
+}
+
+export default List
