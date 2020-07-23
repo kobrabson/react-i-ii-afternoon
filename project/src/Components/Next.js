6ec2a36e0c3857f086f46478eb7177bb81376a76
kobrabson <67922399+kobrabson@users.noreply.github.com>
2020-07-23 15:33:36 -0600

    nextbutton setup

---
+++ project/src/Components/Next.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+const Next = () => {
+    return (
+        <div className = 'Next'>
+            <button 
+            className = 'Next-Button'>
+                Next
+            </button>
+        </div>
+    )
+}
+
+export default Next
