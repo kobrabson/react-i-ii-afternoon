f3634d8e8d0bb34f443e27b6193dd433bd5f4138
kobrabson <67922399+kobrabson@users.noreply.github.com>
2020-07-23 15:28:03 -0600

    ListItem.js setup

---
+++ project/src/Components/ListItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ListItem = () => {
+    return (
+        <div className = 'List-Item'>
+            ListItem.js
+        </div>
+    )
+}
+
+export default ListItem
