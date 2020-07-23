672a64854209ae9463099abdd753f530233d2c94
kobrabson <67922399+kobrabson@users.noreply.github.com>
2020-07-23 15:34:51 -0600

    Previousbutton setup

---
+++ project/src/Components/Previous.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+const Previous = () => {
+    return (
+        <div className = 'Previous'>
+            <button 
+            className = 'Previous-Button'>
+                Previous
+            </button>
+        </div>
+    )
+}
+
+export default Previous
